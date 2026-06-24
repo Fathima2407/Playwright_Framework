c91168ca3dd9a6a86ab46c30d2a452018024ae9d
Fathima2407 <noorefathima07@gmail.com>
2026-06-24 22:01:04 +0530

    Updated Assertions1 test

--- tests/IntegrationTesting/Assertions1.spec.js
+++ tests/IntegrationTesting/Assertions1.spec.js
@@ -13,3 +13,17 @@ test('Assertions', async({page})=>
     await page.locator("#hobbies").check();
     await expect(page.locator("#hobbies")).toBeChecked();
 });
+
+test('Assertions 111111', async({page})=>
+{
+    await page.goto("https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php");
+    await page.locator("#name").fill("Test User");
+    await page.locator("#email").fill("abc@gmail.com");
+    await expect.soft(page.locator("#name")).toHaveValue("Test User");
+
+    // await page.locator("#gender").check();
+    await expect(page.locator("#gender")).not.toBeChecked();
+
+    await page.locator("#hobbies").check();
+    await expect(page.locator("#hobbies")).toBeChecked();
+});
